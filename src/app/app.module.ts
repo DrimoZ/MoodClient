10cde6cf406a7f3f8faa136991bfbca9f863af46
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2023-11-13 18:02:05 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MoodContainerComponent } from './mood-container/mood-container.componen
 import { LoginPageComponent } from './mood-container/login-page/login-page.component';
 import { RegisterPageComponent } from './mood-container/register-page/register-page.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {AppRoutingModule} from "./app-routing.module";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
