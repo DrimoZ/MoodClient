1a3596b7be74d42a52d8070845832eb08f37233d
DrimoZ <drimozbe@gmail.com>
2023-12-07 17:08:24 +0100

    Add Scroll Bar to Profile removed from Global Page

--- src/app/mood-container/main-page/content/profile/profile.module.ts
+++ src/app/mood-container/main-page/content/profile/profile.module.ts
@@ -7,20 +7,23 @@ import { FriendsComponent } from './friends/friends.component';
 import { AccountComponent } from './account/account.component';
 import {NgClass, NgForOf} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 @NgModule({
   declarations: [
     PublicationsComponent,
     FriendsComponent,
     AccountComponent
   ],
-    imports: [
-        ProfileRoutingModule,
-        NgbModule,
-        NgForOf,
-        NgClass,
-        FormsModule,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    ProfileRoutingModule,
+    NgbModule,
+    NgForOf,
+    NgClass,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   providers: [],
 })
 export class ProfileModule { }
