4b6a69e2674aaeaf101247461309c5525fd115eb
Dampsey <44072959+Dampsey@users.noreply.github.com>
2023-12-21 16:33:10 +0100

    Cypress Improvements, grouping and cleaning

--- cypress/e2e/Publications.cy.ts
+++ cypress/e2e/Publications.cy.ts
@@ -1,5 +1,113 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Post a publication', () => {
+  it('Normal Post', () => {
+    cy.connect("martin_p")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\bg.png")
+    cy.get("#PublicationText").type("Sacré plaque !")
+    cy.contains("Publish").click()
   })
+
+  it('Trying to post without image', () => {
+    cy.connect("martin_p")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("#PublicationText").type("Sacré plaque !")
+  });
+
+  it('Trying to post without text', () => {
+    cy.connect("martin_p")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\bg.png")
+  });
+
+  it('Post multiple images', () => {
+    cy.connect("frontend_champ")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\fish.png")
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\Goldorak.jpg")
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\bg.png")
+
+    cy.get("#PublicationText").type("Sacré plaque !")
+
+    cy.contains("Publish").click()
+  });
+
+  it('Modify my images while posting', () => {
+    cy.connect("martin_p")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\fish.png")
+    cy.get('#DeleteThisImage').click()
+  });
+
+  it("Carousel test to look at images i'm posting", () => {
+    cy.connect("martin_p")
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\fish.png")
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\Goldorak.jpg")
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\bg.png")
+
+    cy.get('#RightCarousel').click()
+    cy.get('#RightCarousel').click()
+    cy.get('#LeftCarousel').click()
+    cy.get('#LeftCarousel').click()
+    cy.get('#RightCarousel').click()
+    cy.get('#RightCarousel').click()
+  });
+})
+describe('Delete my publication', () => {
+  it('Delete a publication on my profile', () => {
+    cy.connect("martin_p")
+    cy.wait(500)
+
+    cy.get("#Profile").click()
+    cy.get(".pub_img").first().click()
+    cy.get("#DeleteThisPub").click()
+  });
+
+  it('Delete an image while in my post edition', () => {
+    cy.connect("martin_p")
+    cy.wait(500)
+
+    cy.get("#NavCreatePub").click()
+    cy.wait(500)
+    cy.get("input[type=file]").selectFile("cypress\\downloads\\fish.png")
+
+    cy.get('#DeleteThisImage').click()
+  });
+})
+
+describe('Likes and Comments', () => {
+  it('Like a publication', () => {
+    cy.connect('martin_p')
+    cy.get('#NavNewsFeed').click()
+    cy.get('#BtnLike').click()
+
+  });
+
+  it('Comment a publication', () => {
+    cy.connect('martin_p')
+    cy.get('#NavNewsFeed').click()
+
+    cy.get('#BtnComment').click()
+    cy.get('#AddComment').type('Waw, Les poissons, ils ont tellement de charisme{enter}')
+  });
+
+  it('Delete my comment', () => {
+    cy.connect('martin_p')
+    cy.get('#NavNewsFeed').click()
+
+    cy.get('#BtnComment').click()
+    cy.contains('X').first().click()
+  });
 })
---
+++ cypress/e2e/User.cy.ts
@@ -0,0 +1,115 @@
+describe('Account management', () => {
+  it("Password doesn't match conditions", () => {
+    cy.fixture('infos').then((infos) => {
+      cy.visit(infos.website)
+    })
+    cy.get('#GoToSignUp').click()
+
+    cy.get('#Name').type("Martin Pecheur")
+    cy.get('#Login').type("Martin le M")
+    cy.get('#Password').type("weak")
+    cy.get('#Mail').type("martinlem@martin.be")
+    cy.get('#Birthdate').type("2006-06-10")
+    cy.get('#PasswordConfirmation').type("weak")
+
+    cy.Password("weak#")
+    cy.Password("weak1")
+    cy.Password("longweak#")
+    cy.Password("strong#1")
+  })
+
+  it('Successful account creation', () => {
+    cy.fixture('infos').then((infos) => {
+      cy.visit(infos.website)
+    })
+    cy.get('#GoToSignUp').click()
+
+    cy.get('#Name').type("Martin Anti-Pecheur")
+    cy.get('#Login').type("Martin le AP")
+    cy.get('#Mail').type("martinlem@martin.be")
+    cy.get('#Birthdate').type("2006-06-10")
+    cy.Password("Strong#1")
+    cy.get('#PasswordConfirmation').type("Strong#1")
+
+    cy.get('#BtnSignUp').click()
+    cy.wait(2000)
+  })
+
+  it('Change profile picture', () => {
+    cy.connect('Martin le AP')
+
+    cy.get('#Profile').click()
+    cy.get("#ChangeProfilePicture").click()
+    cy.get('input[type=file]').selectFile('cypress\\downloads\\bg.png')
+    cy.contains('Submit').click()
+
+    cy.get("#ChangeProfilePicture").click()
+    cy.get('input[type=file]').selectFile('cypress\\downloads\\fish.png')
+    cy.contains('Submit').click()
+
+    cy.get("#ChangeProfilePicture").click()
+    cy.get('input[type=file]').selectFile('cypress\\downloads\\Goldorak.jpg')
+    cy.contains('Submit').click()
+  })
+
+  it('Edit my personnal informations', () => {
+    cy.connect('marine0023')
+
+    cy.get('#Profile').click()
+
+    cy.contains('Account').click()
+    cy.wait(1000)
+    cy.contains('Edit Account').click()
+
+    cy.get('#InputFullName').clear()
+    cy.get('#InputFullName').type('Adurian')
+    cy.get('#InputEmail').clear()
+    cy.get('#InputEmail').type('Adurian@Ninja.com')
+    cy.get('#InputBirthDate').clear()
+    cy.get('#InputBirthDate').type('1999-04-11')
+    cy.get('#InputTitle').clear()
+    cy.get('#InputTitle').type('Ninja')
+    cy.get('#InputDescription').clear()
+    cy.get('#InputDescription').type('BAC+5 Ninja')
+
+    cy.contains('Validate Changes').click()
+
+    cy.contains('Edit Account').click()
+
+    cy.get('#InputFullName').clear()
+    cy.get('#InputFullName').type('Martin Anti-Pecheur')
+    cy.get('#InputEmail').clear()
+    cy.get('#InputEmail').type('martinlem@martin.be')
+    cy.get('#InputBirthDate').clear()
+    cy.get('#InputBirthDate').type('2006-06-10')
+    cy.get('#InputTitle').clear()
+    cy.get('#InputTitle').type("L'originel")
+    cy.get('#InputDescription').clear()
+    cy.get('#InputDescription').type('#Hérétique')
+
+    cy.contains('Validate Changes').click()
+  })
+
+  it('Delete my account', () => {
+    cy.connect('Martin le AP')
+    cy.deleteMyAccount()
+  })
+})
+describe('Each way to connect to the website', () => {
+
+  it('Trying to log in but with wrong login', () => {
+    cy.connect("idontexist")
+  })
+
+  it('Trying to log in but with wrong password', () => {
+    cy.fixture('infos').then((infos) => {
+      cy.visit(infos.website)
+    })
+    cy.get('#Login').type('marine0023')
+    cy.get('#Password').type('wrong')
+  })
+
+  it('Log in correctly', () => {
+    cy.connect('marine0023')
+  })
+})
