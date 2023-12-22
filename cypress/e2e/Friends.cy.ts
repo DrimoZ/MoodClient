eb8f9524c47652803b916ec3b048eae037eddd3c
Dampsey <44072959+Dampsey@users.noreply.github.com>
2023-12-22 14:47:02 +0100

    Cypress Friends improvements

--- cypress/e2e/Friends.cy.ts
+++ cypress/e2e/Friends.cy.ts
@@ -52,10 +52,47 @@ describe('Friends management', () => {
 });
 
 describe('Chat with friends', () => {
-  it('Create a group', () => {
+  it("Check if a private conversation exist with Martin, otherwise, create one. FAILS IF IT EXISTS", () => {
     cy.connect('marine0023')
     cy.get('#NavMessages').click()
+
+    cy.get('#PrivateConv').contains('Martin').should('not.exist')
+
+    cy.get('#BtnCreateConversation').click()
+    cy.contains('Martin').click()
+    cy.get('#ConfirmCreateConv').click()
   });
+  it('Check if a group conversation exist, otherwise, create one. FAILS IF IT EXISTS', () => {
+    cy.connect('marine0023')
+    cy.get('#NavMessages').click()
+
+    cy.get('#GroupConv').should('not.exist')
+
+    cy.get('#BtnCreateConversation').click()
+    cy.get('#Friend').click({multiple:true})
+    cy.get('#GroupName').type('All my friends')
+    cy.get('#ConfirmCreateConv').click()
+  });
+
+  it('Send a message in a private conversation', () => {
+    cy.connect('marine0023')
+    cy.get('#NavMessages').click()
+
+    cy.get('#PrivateConv').click()
+    cy.get('#Message').type('Ceci est un test')
+
+    cy.get('#SendMessage').click()
+  });
+
+  it('Check if martin received the message and send a message back', () => {
+    cy.connect('martin_p')
+    cy.get('#NavMessages').click()
+
+    cy.get('#PrivateConv').contains('Marine').click()
+
+    cy.get('')
+  });
+
 
   it('Send a Real Time message', () => {
 
